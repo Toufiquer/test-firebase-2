4c830683176ccb382ad08f370a4204739a0e311e
Toufiquer <toufiquerabc@gmail.com>
2023-01-29 00:46:46 +0600

    design log in and sugn Up

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,7 +1,44 @@
+import { Button, Label, TextInput } from "flowbite-react";
 import React from "react";
+import { Link } from "react-router-dom";
+import Social from "./Social";
 
 const SignUp = () => {
-  return <div className={`min-h-screen bg-blue-200 p-4`}>Sign Up</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submit");
+  };
+  return (
+    <div className={`min-h-screen bg-blue-200 p-4 grid grid-cols-1 md:grid-cols-2`}>
+      <div className="p-4 border-2 border-solid border-blue-600 w-[480px] h-[360px] rounded mx-auto ">
+        <h2 className={`text-center text-2xl mb-2 font-semibold`}>Please Registration</h2>
+        <form onClick={handleSubmit} className="flex flex-col gap-4">
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="email1" value="Your email" />
+            </div>
+            <TextInput id="email1" type="email" placeholder="name@flowbite.com" required={true} className={`w-full text-black`} />
+          </div>
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="password1" value="Your password" />
+            </div>
+            <TextInput id="password1" type="password" required={true} />
+          </div>
+          <Label htmlFor="agree">
+            Already Have an Account,{" "}
+            <Link to="/logIn" className="text-blue-600 hover:underline dark:text-blue-500">
+              Continue With LogIn
+            </Link>
+          </Label>
+          <Button type="submit" className={`border-2 border-solid border-purple-600 mb-2 p-2 text-black cursor-pointer flex items-center justify-center rounded font-semibold hover:text-white hover:bg-purple-600 duration-300 bg-purple-500 w-full`}>
+            Log In
+          </Button>
+        </form>
+      </div>
+      <Social></Social>
+    </div>
+  );
 };
 
 export default SignUp;
---
+++ src/components/Social.js
@@ -0,0 +1,22 @@
+import { Button } from "flowbite-react";
+import React from "react";
+import googleIcon from "./icon/google.png";
+import faceBookIcon from "./icon/facebook.png";
+import githubIcon from "./icon/github.png";
+const Social = () => {
+  return (
+    <div className={`container mx-auto p-4 flex flex-col gap-4 max-w-[480px]`}>
+      <Button className={`border-2 border-solid border-purple-600 mb-2 p-2 text-black cursor-pointerw-full flex items-center justify-center rounded font-semibold hover:text-white hover:bg-purple-600 duration-300 bg-purple-500`}>
+        <img className={`h-8 w-8 mr-2 bg-slate-300 rounded-xl`} src={googleIcon} alt="Google Icon" /> Continue With Google
+      </Button>
+      <Button className={`border-2 border-solid border-purple-600 mb-2 p-2 text-black cursor-pointerw-full flex items-center justify-center rounded font-semibold hover:text-white hover:bg-purple-600 duration-300 bg-purple-500`}>
+        <img className={`h-8 w-8 mr-2 bg-slate-300 rounded-xl`} src={faceBookIcon} alt="Google Icon" /> Continue With FaceBook
+      </Button>
+      <Button className={`border-2 border-solid border-purple-600 mb-2 p-2 text-black cursor-pointerw-full flex items-center justify-center rounded font-semibold hover:text-white hover:bg-purple-600 duration-300 bg-purple-500`}>
+        <img className={`h-8 w-8 mr-2 bg-slate-300 rounded-xl`} src={githubIcon} alt="Google Icon" /> Continue With GitHub
+      </Button>
+    </div>
+  );
+};
+
+export default Social;
